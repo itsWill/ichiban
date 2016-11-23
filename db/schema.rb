d5517aa214b6b59bf7d156dda3bd3066984bd51f
itsWill <guilhermerpmansur@gmail.com>
2016-11-22 19:25:14 -0500

    changed db to use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161122234943) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
@@ -19,7 +22,7 @@ ActiveRecord::Schema.define(version: 20161122234943) do
     t.string   "status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_todos_on_user_id"
+    t.index ["user_id"], name: "index_todos_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
